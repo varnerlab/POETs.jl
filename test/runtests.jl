0a793ca71b92f30427e896fd25da4a73348c2d32
Jeffrey Varner <jdvarner@purdue.edu>
2016-05-09 18:59:39 -0400

    Update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,1 @@
-using POETs
-include("binh_korn_function.jl")
-
-(EC,PC,RA) = estimate_ensemble(objective_function,neighbor_function,acceptance_probability_function,2.0.*(1+0.5*randn(2));rank_cutoff=4,maximum_number_of_iterations=40)
+include("test_binh_korn.jl")
---
+++ test/test_binh_korn.jl
@@ -0,0 +1,5 @@
+using POETs
+using PyPlot
+include("binh_korn_function.jl")
+
+(EC,PC,RA) = estimate_ensemble(objective_function,neighbor_function,acceptance_probability_function,2.0.*(1+0.5*randn(2));rank_cutoff=4,maximum_number_of_iterations=40)
